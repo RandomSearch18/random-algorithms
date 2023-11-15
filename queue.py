91ffc624e856458087ff241f16c3b889466419f3
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-11-15 14:17:42 +0000

    Implement Queue#is_full()

--- queue.py
+++ queue.py
@@ -5,6 +5,20 @@ class Queue:
         self._length = length
         self._queue = [self.EMPTY_SLOT] * length
         self._next_free_slot = 0
+        self._start = 0
+        self._end = 0
+
+    def is_full(self):
+        if self._start == 0 and self._end == self._length - 1:
+            # The queue is full with no wrap-around
+            return True
+
+        if self._end == self._start - 1:
+            # The queue has wrapped around and the end pointer is just before the start
+            return True
+
+        # At least 1 free space should be available
+        return False
 
     def calculate_next_free_slot(self, start_from: int) -> int:
         current_index = start_from + 1
@@ -18,8 +32,12 @@ class Queue:
         return self.calculate_next_free_slot(0)
 
     def enqueue(self, item):
+        if self.is_full():
+            raise RuntimeError(f"Queue is full! (Max {self._length} items)")
+        
         current_slot = self._next_free_slot
         self._queue[current_slot] = item
+        self._end += 1
 
         self._next_free_slot = self.calculate_next_free_slot(current_slot)
         return current_slot
