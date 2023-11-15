42211e7ee8885d66a3c900aabe09b2bb5ac35c80
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-11-15 14:54:19 +0000

    Continue working on the linked list

--- linked_list.py
+++ linked_list.py
@@ -7,7 +7,7 @@ class LinkedList:
         """Returns the index of the item that should be before the provided new item"""
         result = -1
         next_index = 0
-        current_index = 0
+        current_index = 0  # First entry is at self._list[0]
         while next_index is not None:
             current_item, next_index = self._list[current_index]
             if current_item > new_item:
@@ -21,12 +21,29 @@ class LinkedList:
         # so return the final item in the list
         return len(self._list) - 1
 
-    def append(self):
-        pass
+    def append(self, new_item):
+        previous_item_index = self.find_item_before(new_item)
+        previous_item, old_next_item = self._list[previous_item_index]
+        #print("Prev entry", self._list[previous_item_index])
+
+        new_item_index = previous_item_index + 1
+
+        # Update the item before the new one to point to the new item
+        self._list[previous_item_index][1] = new_item_index
+
+        new_entry = [new_item, old_next_item]
+        self._list.insert(new_item_index, new_entry)
+
+        return new_item_index
 
 
 linked_list = LinkedList()
-print(linked_list.find_item_before("Aadamn"))
-print(linked_list.find_item_before("Azdamn"))
-print(linked_list.find_item_before("Madamn"))
-print(linked_list.find_item_before("Zzdamn"))
+print(linked_list._list)
+linked_list.append("Aadamn")
+print(linked_list._list)
+linked_list.append("Azdamn")
+print(linked_list._list)
+linked_list.append("Madamn")
+print(linked_list._list)
+linked_list.append("Zzdamn")
+print(linked_list._list)
