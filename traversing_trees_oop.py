41315c007401d448592199e1b4485dbe44829139
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2023-12-04 11:38:25 +0000

    Finish off object-oriented tree traversal

--- traversing_trees_oop.py
+++ traversing_trees_oop.py
@@ -1,52 +1,48 @@
 # From Class Notebook, "Python Tree Implementation"
 from __future__ import annotations
+from typing import Optional
 
 
 class Node:
 
-    def __init__(self, value, left: Node, right: Node) -> None:
+    def __init__(self, value, left: Optional[Node] = None, right: Optional[Node] = None) -> None:
         self.value = value
         self.left = left
         self.right = right
 
+    def __str__(self) -> str:
+        return f"{self.value}"
+
     def traverse(self):
-        if self.left: traverse(self.left)
-        print(self.value)
-        if self.right: traverse(self.right)
+        """Traverses through the tree, returning an array of values"""
+        result_values = []
+        if self.left:
+            result_values.extend(self.left.traverse())
 
+        result_values.append(self.value)
 
-def create_node(value):
-    return {'value': value, 'left': None, 'right': None}
+        if self.right:
+            result_values.extend(self.right.traverse())
 
+        return result_values
 
-def insert_node(root, value):
-    if root is None:
-        return create_node(value)
-    else:
-        if value < root['value']:
-            root['left'] = insert_node(root['left'], value)
+    def insert_child_value(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = Node(value)
+                return
+            self.left.insert_child_value(value)
         else:
-            root['right'] = insert_node(root['right'], value)
-        return root
+            if not self.right:
+                self.right = Node(value)
+                return
+            self.right.insert_child_value(value)
 
-
-def traverse(root):
-    """Performs in-roder traversal, printing the outputs"""
-    # Traverse left subtree
-    if root['left']: traverse(root['left'])
-    # Traverse root
-    print(root['value'])
-    # Traverse right subtree
-    if root['right']: traverse(root['right'])
-
-
-# Example usage:
-tree_root = None
-values_to_insert = [5, 3, 7, 2, 4, 6, 8]
+tree_root = Node(5)
+values_to_insert = [3, 7, 2, 4, 6, 8]
 
 for value in values_to_insert:
-    tree_root = insert_node(tree_root, value)
+    tree_root.insert_child_value(value)
 
-print(tree_root)
-
-traverse(tree_root)
+for value in tree_root.traverse():
+    print(value)
