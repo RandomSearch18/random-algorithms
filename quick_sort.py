ad756e817352365fa2294fa59d7c11124646cfd3
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-05-16 08:35:39 +0000

    Implement quick sort

---
+++ quick_sort.py
@@ -0,0 +1,24 @@
+def quick_sort(array: list):
+    if len(array) <= 1:
+        print(f"Array {array} is already sorted")
+        return array
+    print(f"Sorting {array}")
+    pivot = len(array) // 2
+    left = 0
+    right = len(array) - 1
+    while left <= right and right != pivot:
+        while array[left] < array[pivot]:
+            left += 1
+        while array[right] > array[pivot]:
+            right -= 1
+        print(f"Swapped <{left}>{array[left]} with <{right}>{array[right]}")
+        array[left], array[right] = array[right], array[left]
+    array[right], array[pivot] = array[pivot], array[right]
+
+    return quick_sort(array[: pivot - 1]) + [array[pivot]] + quick_sort(array[pivot:])
+
+
+# print(quick_sort([3, 6, 8, 10, 1, 2, 1]))
+# print(quick_sort([1, 0, 0, 1, 1, 0, 1]))
+# print(quick_sort([1, 2, 3, 4, 5]))
+print("Sorted:", quick_sort([5, 4, 3, 2, 1]))
