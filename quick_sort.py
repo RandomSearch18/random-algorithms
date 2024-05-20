0082a654ec47ca704564b6ca5f04f104a703def1
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2024-05-20 10:41:37 +0000

    Bugfixing attempts on my merge sort impl from last lesson

--- quick_sort.py
+++ quick_sort.py
@@ -6,10 +6,10 @@ def quick_sort(array: list):
     pivot = len(array) // 2
     left = 0
     right = len(array) - 1
-    while left <= right and right != pivot:
-        while array[left] < array[pivot]:
+    while left <= right and right != pivot and left != pivot:
+        while array[left] < array[pivot] and array[left] != array[pivot]:
             left += 1
-        while array[right] > array[pivot]:
+        while array[right] > array[pivot] and array[right] != array[pivot]:
             right -= 1
         print(f"Swapped <{left}>{array[left]} with <{right}>{array[right]}")
         array[left], array[right] = array[right], array[left]
@@ -18,7 +18,7 @@ def quick_sort(array: list):
     return quick_sort(array[: pivot - 1]) + [array[pivot]] + quick_sort(array[pivot:])
 
 
-# print(quick_sort([3, 6, 8, 10, 1, 2, 1]))
-# print(quick_sort([1, 0, 0, 1, 1, 0, 1]))
-# print(quick_sort([1, 2, 3, 4, 5]))
-print("Sorted:", quick_sort([5, 4, 3, 2, 1]))
+print(quick_sort([3, 6, 8, 10, 1, 2, 1]))
+print(quick_sort([1, 0, 0, 1, 1, 0, 1]))
+print(quick_sort([1, 2, 3, 4, 5]))
+print(quick_sort([5, 4, 3, 2, 1]))
